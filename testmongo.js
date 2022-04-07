846858c9fc15b1cbd0dff6c42d62f103ebcb135a
Jonathan Martel <jonathmartel@gmail.com>
2022-04-06 21:02:13 -0400

    ajouter - apres pause

--- testmongo.js
+++ testmongo.js
@@ -1,6 +1,5 @@
-// https://mongodb.github.io/node-mongodb-native/api-generated/mongoclient.html#connect
 // https://www.mongodb.com/docs/manual/crud/
-
+//https://www.mongodb.com/docs/drivers/node/current/
 
 const express = require("express");
 const mongoClient = require("mongodb").MongoClient;
@@ -9,7 +8,7 @@ const app = express();
 
 
 app.get("/ajouter", (req, res)=>{
-    mongoClient.connect("mongodb://localhost:27017/", (err, db)=>{
+    mongoClient.connect("mongodb://127.0.0.1:27017/", (err, db)=>{
         if(err) {
             res.send("erreur 1");
             throw err;
@@ -35,7 +34,7 @@ app.get("/biere", (req, res )=>{
     //let test = {id: 123}
     //res.send(test);
 
-    mongoClient.connect("mongodb://localhost:27017/", (err, db)=>{
+    mongoClient.connect("mongodb://127.0.0.1:27017/", (err, db)=>{
         if(err) {
             res.send("erreur 1");
             throw err;
